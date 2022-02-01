c759fad4ed5f3bac5c3fbe22d52a854245f5dbfc
Marina-93 <marina.ua93@gmail.com>
2022-02-01 15:06:26 +0200

    node 3.0.1

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
-const app = require('./app')
+// const app = require('./app')
 
-app.listen(3000, () => {
-  console.log(`Server running. Use our API on port: ${PORT}`)
-})
+// app.listen(3000, () => {
+//   console.log(`Server running. Use our API on port: ${PORT}`)
+// })
+
+const mongoose = require('mongoose');
+
+const DB_HOST =
+  'mongodb+srv://mari:mongodb06@cluster0.49um5.mongodb.net/db-contacts?retryWrites=true&w=majority';
+mongoose.connect(DB_HOST)
+  .then(() => console.log('Database connection successful'))
+  .catch((error) => console.log(error.message));
